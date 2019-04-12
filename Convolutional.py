e4e7c6fbcc6d368ce28f04f17cb1dcf14c4eec61
unknown <unmukt.deswal@gmail.com>
2019-04-12 14:51:51 +0200

    Modified the labels in the dataset. Created CNN model.

--- Convolutional.py
+++ Convolutional.py
@@ -1,18 +1,22 @@
 import tensorflow as tf
+import keras
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 import numpy as np
 import imageio as ima
-import visvis as vv
+#import visvis as vv
 import glob
 import random
+
 
 #Load Data
 trainDataPath = "./data/seg_train/"
 testDataPath = "./data/seg_test/"
 
-labels = ["buildings","forest","glacier","mountain","sea","street"]
+labels = ["buildings","forests","glaciers","mountains","seas","streets"]
 
-def train(data):
+
+def shuffle(data):
     labels = []
     features = []
 
@@ -20,43 +24,92 @@ def train(data):
     for dataPoint in data:
         labels.append(dataPoint[0])
         features.append(dataPoint[1])
-    #print(key)
-    #print(data[key])
-    print(labels)
-
+    
+    return labels, features
     
 
-def convolutionStep(filterData, weights, bias):
-    data = np.dot(filterData, weights.T)
-    output = np.sum(data) + float(bias)
-    return output
+def modelCNN(data, label):
+    #CNN Model Architecture:
+    model = tf.keras.models.Sequential()
+    
+    #First Layer:
+    conv_1 = Conv2D(
+            36, kernel_size = (5, 5),
+            strides = (2, 2), padding = 'valid',
+            activation = 'relu', bias_initializer = 'zeros',
+            input_shape = (150, 150, 1))
+    
+    pool_1 = MaxPooling2D(2, 2)
+    
+    model.add(conv_1)
+    model.add(pool_1)
+    
+    #Second Layer:
+    conv_2 = Conv2D(
+            72, kernel_size = (5, 5),
+            strides = (2, 2), padding = 'valid',
+            activation = 'relu', bias_initializer = 'zeros')
+    
+    pool_2 = MaxPooling2D(2, 2)
+    
+    model.add(conv_2)
+    model.add(pool_2)
+    
+    #Third Layer:
+    conv_3 = Conv2D(
+            72, kernel_size = (5, 5),
+            strides = (2, 2), padding = 'valid',
+            activation = 'relu', bias_initializer = 'zeros')
+    
+    pool_3 = MaxPooling2D(2, 2)
+    
+    model.add(conv_3)
+    model.add(pool_3)
+    
+    #Fully Connected Layers:
+    fConnect = Flatten()
+    fConnect_4 = Dense(300, activation = 'relu')
+    fConnect_5 = Dense(6, activation = 'softmax')
+    
+    model.add(fConnect)
+    model.add(fConnect_4)
+    model.add(fConnect_5)
 
-def pads(X, size):
-    pad = np.pad(X, ((0,0), (size,size), (size,size)), mode = 'constant', constant_values = (0,0))
-    return pads
 
-def forwardPropagation(data, weights):
-    w1, w2 = initializeWeights()
+    #Run Model:
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+    accuracy = model.fit(data, label, epochs = 5)
+    
+    return accuracy
+    
 
 def main():
     trainData = []
-    testData = {}
+    testData = []
+    print("The training data consists of :")
+    
     for label in labels:
         trainDatap = trainDataPath + label
         trainFiles = glob.glob(trainDatap + "/*.jpg")
-
-        for trainFile in trainFiles:
-            picture = [label,ima.imread(trainFile)/256]
-            trainData.append(picture)
-        print(label)
-        print(len(trainData))
-
-        testData.update({label:[]})
         testDatap = testDataPath + label
         testFiles = glob.glob(testDatap + "/*.jpg")
 
+        for trainFile in trainFiles:
+            trainPicture = [ label, ima.imread(trainFile)/256 ]
+            trainData.append(trainPicture)
+            
         for testFile in testFiles:
-            testData[label].append(ima.imread(testFile)/256)
+            testPicture =  [ label, ima.imread(testFile)/256 ]
+            testData.append(testPicture)
+        
+        print(str(len(trainData)) + ' ' + label)
 
-    train(trainData)
+    inputData, outputData = shuffle(trainData)
+    
+    modelCNN(inputData, outputData)
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------
 main()
